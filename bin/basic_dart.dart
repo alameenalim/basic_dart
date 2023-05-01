15d1882e3de955be4f7c37187d30d311f2b66d49
alameenalim <alameenalim007@gmail.com>
2023-05-01 16:27:50 +0530

    first commit

---
+++ bin/basic_dart.dart
@@ -0,0 +1,17 @@
+// main fn is the entry point
+//
+import 'dart:io';
+
+void main() {
+  print("hello world");
+
+  //datatypes in dart
+  /// int double 
+  /// num int and double
+  /// string 
+  /// var
+  // Input value
+  var a = stdin.readLineSync();
+  stdout.write(a);
+  print('Hello $a');
+}
